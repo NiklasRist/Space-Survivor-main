0599a1018ae097a9af114104e0cc4bf21d14ae30
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2023-06-18 18:03:58 +0200

    LAN...

---
+++ lan_communication.py
@@ -0,0 +1,10 @@
+from dhcppython import client as cl
+import pydhcplib
+class lan_communication:
+    def __init__(self) -> None:
+        self.dhcp=cl.DHCPClient()
+        self.ip=0
+    
+    def setup_connection(self):
+        self.dhcp.send_discover()
+        
