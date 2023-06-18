a37f36580563ecdbe142467a35b8d1af7eb48281
NiklasRistCds <79253498+NiklasRistCds@users.noreply.github.com>
2023-06-18 18:11:33 +0200

    Socket LAN Datenübertragung...

--- lan_communication.py
+++ lan_communication.py
@@ -1,10 +1,9 @@
 from dhcppython import client as cl
-import pydhcplib
 class lan_communication:
     def __init__(self) -> None:
         self.dhcp=cl.DHCPClient()
         self.ip=0
     
     def setup_connection(self):
-        self.dhcp.send_discover()
+        self.dhcp.send_discover("FFFFFFFFFFFF", )
         
